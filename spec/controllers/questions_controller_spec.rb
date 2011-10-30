3dbf3b52c0154ea38ef823a8c50c2b26df069c57
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-30 19:45:11 +0400

    questions controller -> create method tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,7 +3,18 @@ require 'spec_helper'
 describe QuestionsController do
 	it 'should show open questions' do
 		get :index
-		assigns(:open_questions).count.should == Question.open.count
-		assigns(:closed_questions).count.should == Question.closed.count
+		assigns[:open_questions].count.should == Question.open.count
+		assigns[:closed_questions].count.should == Question.closed.count
+	end
+
+	it 'should create a new question on POST data' do
+		post 'create', :question => { :body => 'Question1' }
+		assigns[:question].body.should == 'Question1'
+		Question.count.should == 1
+	end
+
+	it 'should return Question instance with validation errors explained' do
+		post 'create'
+		assigns[:question].errors.full_messages.first.should == "Body can't be blank"
 	end
 end
