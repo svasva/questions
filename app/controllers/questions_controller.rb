a166dfc45b10779d72cdb1e95ef34cacb01d44f0
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-30 18:34:00 +0400

    QuestionsController -> refactor create/update methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+	respond_to :html, :js
+
 	def index
 		# NOT optimized for performance
 		# TODO: add pagination, etc
@@ -13,29 +15,13 @@ class QuestionsController < ApplicationController
 
 	def create
     @question = Question.new(params[:question])
-
-    respond_to do |format|
-      if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
-        format.json { render json: @question, status: :created, location: @question }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
-    end
+    @question.save
+    respond_with @question, :location => questions_url
 	end
 
 	def update
     @question = Question.find(params[:id])
-
-    respond_to do |format|
-      if @question.update_attributes(params[:question])
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
-    end
+    @question.update_attributes(params[:question])
+    respond_with @question, :location => questions_url
 	end
 end
