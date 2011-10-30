3ca83dfa04fe1266b84b3dd4b609632ff9c4eaa9
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-30 15:27:33 +0400

    Question -> scopes test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,4 +6,13 @@ describe Question do
 		q = Question.new
 		q.save.should == false
 	end
+
+	it 'should distribute to right scopes' do
+		q = Question.create(:body => 'Is this a question?')
+		Question.open.count.should == 1
+
+		q.update_attributes(:answer => 'Sure it is!')
+		Question.open.count.should == 0
+		Question.closed.count.should == 1
+	end
 end
