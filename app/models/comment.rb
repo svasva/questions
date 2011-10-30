d7256a87faca20774ca0fd49bfb081fa94ce1c18
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-30 15:20:36 +0400

    Comment -> test & validation for open question

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :question
+  validate :validate_parent
+
+  protected
+
+  def validate_parent
+  	errors[:base] << "Cant comment unanswered question" unless self.question.answer
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+	it 'shouldnt save comments to an open question' do
+		q = Question.new(:body => "Does cow say 'moo'?")
+		q.save
+		c = q.comments.new(:body => 'Right answer!')
+		c.save.should == false
+	end
 end
