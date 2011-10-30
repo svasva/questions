a082c024f6443cc2f02c5f842083d909fc69c7e5
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-30 17:15:32 +0400

    QuestionsController -> create/update methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,36 @@ class QuestionsController < ApplicationController
 		@open_questions = Question.open.all
 		@closed_questions = Question.closed.all
 	end
+
+	def show
+		redirect_to :action => :index
+	end
+
+	def create
+    @question = Question.new(params[:question])
+
+    respond_to do |format|
+      if @question.save
+        format.html { redirect_to @question, notice: 'Question was successfully created.' }
+        format.json { render json: @question, status: :created, location: @question }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
+	end
+
+	def update
+    @question = Question.find(params[:id])
+
+    respond_to do |format|
+      if @question.update_attributes(params[:question])
+        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @question.errors, status: :unprocessable_entity }
+      end
+    end
+	end
 end
