7236b201089c1035b3a97d5485b6741942310202
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-30 15:15:03 +0400

    Question -> first validation & test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+	it 'shouldnt save wuthout a body' do
+		q = Question.new
+		q.save.should == false
+	end
 end
