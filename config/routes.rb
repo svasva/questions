67aead33d56c82139871c5b9ae8862a8211ee923
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-30 15:50:45 +0400

    default route to QuestionsController#index

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,9 @@ Questions::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
+  resources :questions
+  root :to => 'questions#index'
 
   # See how all your routes lay out with "rake routes"
 
