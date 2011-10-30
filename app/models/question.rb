51078f8d9454c490a014c4296419429daf196766
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-30 15:49:43 +0400

    Question -> notes

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,8 @@ class Question < ActiveRecord::Base
 	has_many :comments
 	validates_presence_of :body
 
+	# NOT optimized for performance
+	# TODO: we should have a bool variable like :closed and an index on it
 	scope :open, where(:answer => nil)
 	scope :closed, where('answer IS NOT NULL')
 end
