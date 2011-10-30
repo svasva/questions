67f43c8735930ca87f771b91e003097e4e199ede
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-30 20:09:38 +0400

    remove excess logging

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,6 @@ class CommentsController < ApplicationController
 	def create
     @comment = Comment.new(params[:comment])
     @comment.question_id = params[:question_id]
-    logger.info @comment.inspect
     @comment.save
     respond_with @comment, :location => questions_url
 	end
