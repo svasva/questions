4f8d2b42b4c60c855c3357719db80fd94c634e13
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-30 15:22:26 +0400

    fix a typo in questions spec

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Question do
   #pending "add some examples to (or delete) #{__FILE__}"
-	it 'shouldnt save wuthout a body' do
+	it 'shouldnt save without a body' do
 		q = Question.new
 		q.save.should == false
 	end
