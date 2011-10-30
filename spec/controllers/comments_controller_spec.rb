ab094d02e78a0c09393715d4703b055921d43a42
Vitaly A. Sorokin <erundook@gmail.com>
2011-10-30 20:10:12 +0400

    CommentsController tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe CommentsController do
+	it 'should create a new question on POST data' do
+		q = Question.create(:body => 'question', :answer => 'answer')
+		post 'create', :question_id => q.id, :comment => { :body => 'Comment1' }
+		assigns[:comment].valid?.should == true
+		assigns[:comment].body.should == 'Comment1'
+		Comment.count.should == 1
+	end
 
+	it 'should return Comment instance with validation errors explained' do
+		q = Question.create(:body => 'question1')
+		post 'create', :question_id => q.id, :comment => { :body => 'Comment1'}
+		assigns[:comment].valid?.should == false
+		assigns[:comment].errors.full_messages.first.should == "Cant comment unanswered question"
+	end
 end
